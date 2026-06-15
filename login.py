ee3761a35d063bb4e90100579a9788e10492dfd7
neko <myworldinend@gmail.com>
2026-06-15 21:41:36 +0500

    login basic code

---
+++ login.py
@@ -0,0 +1,8 @@
+usr_name="Ali"
+usr_pass="p@ssw0rd123"
+a=input("Enter User name :")
+b=input("Enter Password :")
+if usr_name==a and usr_pass==b :
+    print("successfully login")
+else :
+    print("wrong password or user name entered ")
